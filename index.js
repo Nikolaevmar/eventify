0112b38ea31b1b178685f6a8399abd2b9d1d1eb9
Nikolaevmar <marioniko001@gmail.com>
2021-09-23 14:57:22 +0300

    Connected the database

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 const express = require("express");
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost:27017/eventify", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+  console.log("Database is connected");
+});
 
 const app = express();
 const path = require("path");
