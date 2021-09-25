f18b2e66035788bdc8bbdf8dcbe31c86efe3c66d
Nikolaevmar <marioniko001@gmail.com>
2021-09-25 13:34:37 +0300

    fixed a typo

--- models/event.js
+++ models/event.js
@@ -1,11 +1,11 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const EventSchema = new Schema({
-    title: String,
-    ticketPrice: String,
-    description: String,
-    localtion: String,
+  title: String,
+  ticketPrice: String,
+  description: String,
+  location: String,
 });
 
-module.exports = mongoose.model('Event', EventSchema);
+module.exports = mongoose.model("Event", EventSchema);
