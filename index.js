b6e79cfaa9031351019a53a146148656f5ff096c
Nikolaevmar <marioniko001@gmail.com>
2021-09-26 15:19:50 +0300

    fixed a typo

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
-const engine = require("ejs-mate");
+const ejsMate = require("ejs-mate");
 const methodOverride = require("method-override");
 const Event = require("./models/event");
 
