4c48d9be47ea31741469c485914038ce72d8e759
Nikolaevmar <marioniko001@gmail.com>
2021-10-06 17:36:55 +0300

    added the registration functionality

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,26 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const User = require('../models/user');
+const catchAsync = require("../utilities/catchAsync");
+const User = require("../models/user");
 
-router.get('/register' ,(req,res) =>{
-    res.render('users/register');
-})
-router.post('/register', async(req,res) => {
-    
-})
+router.get("/register", (req, res) => {
+  res.render("users/register");
+});
+router.post(
+  "/register",
+  catchAsync(async (req, res) => {
+    try {
+      const { email, username, password } = req.body;
+      const user = new User({ email, username });
+      const registeredUser = await User.register(user, password);
+    } catch (e) {
+      req.flash("error", e.message);
+      res.redirect("register");
+    }
 
+    req.flash("success", "Welcome to Eventify!");
+    res.redirect("/events");
+  })
+);
 
 module.exports = router;
