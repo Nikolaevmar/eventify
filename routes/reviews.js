43e1f6a52c8debc37720fda29c660d51fccd02de
Nikolaevmar <marioniko001@gmail.com>
2021-10-08 21:10:31 +0300

    added the reviews controller

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,38 +1,15 @@
 const express = require("express");
 const router = express.Router({ mergeParams: true });
+const reviews = require("../controllers/reviews");
 const { validateReview, isLoggedIn, isReviewAuthor } = require("../middleware");
 const catchAsync = require("../utilities/catchAsync");
 
-const Event = require("../models/event");
-const Review = require("../models/review");
-
-router.post(
-  "/",
-  isLoggedIn,
-  validateReview,
-  catchAsync(async (req, res) => {
-    const event = await Event.findById(req.params.id);
-    const review = new Review(req.body.review);
-    review.author = req.user._id;
-    event.reviews.push(review);
-    await review.save();
-    await event.save();
-    req.flash("success", "Created a new review!");
-    res.redirect(`/events/${event._id}`);
-  })
-);
+router.post("/", isLoggedIn, validateReview, catchAsync(reviews.createReview));
 
 router.delete(
   "/:reviewId",
   isLoggedIn,
   isReviewAuthor,
-  catchAsync(async (req, res) => {
-    const { id, reviewId } = req.params;
-    await Event.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
-    await Review.findByIdAndDelete(reviewId);
-    req.flash("success", "Successfully deleted review!");
-    res.redirect(`/events/${id}`);
-  })
-);
+  catchAsync(reviews.deleteReview));
 
 module.exports = router;
