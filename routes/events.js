13c04fe125f4d6be5934cd08d8f41aadb72dfff3
Nikolaevmar <marioniko001@gmail.com>
2021-10-08 21:24:50 +0300

    restructured routes

--- routes/events.js
+++ routes/events.js
@@ -4,34 +4,24 @@ const events = require("../controllers/events");
 const catchAsync = require("../utilities/catchAsync");
 const { isLoggedIn, isAuthor, validateEvent } = require("../middleware");
 
-router.get("/", catchAsync(events.index));
+router
+  .route("/")
+  .get(catchAsync(events.index))
+  .post(isLoggedIn, validateEvent, catchAsync(events.createEvent));
 
 router.get("/new", isLoggedIn, events.renderNewForm);
 
-router.post("/", isLoggedIn, validateEvent, catchAsync(events.createEvent));
-
-router.get("/:id", catchAsync(events.showEvent));
+router
+  .route("/id")
+  .get(catchAsync(events.showEvent))
+  .put(isLoggedIn, isAuthor, validateEvent, catchAsync(events.updateEvent))
+  .delete(isLoggedIn, isAuthor, catchAsync(events.deleteEvent));
 
 router.get(
   "/:id/edit",
   isLoggedIn,
   isAuthor,
   catchAsync(events.renderEditForm)
-);
-
-router.put(
-  "/:id",
-  isLoggedIn,
-  isAuthor,
-  validateEvent,
-  catchAsync(events.updateEvent)
-);
-
-router.delete(
-  "/:id",
-  isLoggedIn,
-  isAuthor,
-  catchAsync(events.deleteEvent)
 );
 
 module.exports = router;
