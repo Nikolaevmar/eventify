c41623ebff3c2cddb49962129f8a72d51030d914
Nikolaevmar <marioniko001@gmail.com>
2021-10-10 21:29:52 +0300

    added multer middleware to partly handle image uploads

--- routes/events.js
+++ routes/events.js
@@ -2,13 +2,17 @@ const express = require("express");
 const router = express.Router();
 const events = require("../controllers/events");
 const catchAsync = require("../utilities/catchAsync");
+const multer = require("multer");
+const upload = multer({ dest: "uploads/" });
 const { isLoggedIn, isAuthor, validateEvent } = require("../middleware");
-
 
 router
   .route("/")
   .get(catchAsync(events.index))
-  .post(isLoggedIn, validateEvent, catchAsync(events.createEvent));
+  .post(upload.single("image"), (req, res) => {
+    res.send(req.body);
+  });
+// .post(isLoggedIn, validateEvent, catchAsync(events.createEvent));
 
 router.get("/new", isLoggedIn, events.renderNewForm);
 
