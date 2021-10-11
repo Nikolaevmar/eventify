e370dfdb56d574f3f4e8f2525b44c39ea3967028
Nikolaevmar <marioniko001@gmail.com>
2021-10-11 16:14:06 +0300

    set up the map to show up

---
+++ public/javascript/showMap.js
@@ -0,0 +1,8 @@
+mapboxgl.accessToken =
+  "pk.eyJ1IjoibTNyY2hlbCIsImEiOiJja3VtajgwaTMwazk3MzJwZnA1NWI2Mmk5In0.5dHaComs63mxUr7HdMSINw";
+const map = new mapboxgl.Map({
+  container: "map",
+  style: "mapbox://styles/mapbox/streets-v11",
+  center: [-74.5, 40],
+  zoom: 4,
+});
