9daaaefea0a917bab0fa98739e27d78362e66808
Nikolaevmar <marioniko001@gmail.com>
2021-10-11 13:45:45 +0300

    fixed an error with the image thumbnails

--- models/event.js
+++ models/event.js
@@ -2,10 +2,15 @@ const mongoose = require("mongoose");
 const Review = require("./review");
 const Schema = mongoose.Schema;
 
+const ImageSchema = new Schema({ url: String, filename: String });
+ImageSchema.virtual("thumbnail").get(function () {
+  return this.url.replace("/upload", "/upload/w_150/");
+});
+
 const EventSchema = new Schema({
   title: String,
   description: String,
-  images: [{ url: String, filename: String }],
+  images: [ImageSchema],
   ticketPrice: Number,
   date: String,
   location: String,
