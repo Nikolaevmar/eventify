c52289127cf472c45c34ccdf5276c94bfd88e4ea
Nikolaevmar <marioniko001@gmail.com>
2021-10-12 00:10:08 +0300

    added a popup when clicking on the location on the map

--- public/javascript/showMap.js
+++ public/javascript/showMap.js
@@ -4,9 +4,14 @@ const map = new mapboxgl.Map({
   container: "map",
   style: "mapbox://styles/mapbox/streets-v11",
   center: event.geometry.coordinates,
-  zoom: 4,
+  zoom: 10,
 });
 
 new mapboxgl.Marker()
 .setLngLat(event.geometry.coordinates)
+.setPopup(new mapboxgl.Popup({offset: 25})
+.setHTML(
+`<h2>${event.title}`
+ )
+)
 .addTo(map);
