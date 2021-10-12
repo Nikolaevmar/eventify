c5d97b07562c67348836d6864e811e96d1008d87
Nikolaevmar <marioniko001@gmail.com>
2021-10-12 11:40:06 +0300

    displayed current events on the map

--- public/javascript/clusterMap.js
+++ public/javascript/clusterMap.js
@@ -2,7 +2,7 @@ mapboxgl.accessToken = 'pk.eyJ1IjoibTNyY2hlbCIsImEiOiJja3VtajgwaTMwazk3MzJwZnA1N
 const map = new mapboxgl.Map({
 container: 'map',
 style: 'mapbox://styles/mapbox/dark-v10',
-center: [-103.5917, 40.6699],
+center: [23.319941,42.698334],
 zoom: 3
 });
  
@@ -14,7 +14,7 @@ map.addSource('earthquakes', {
 type: 'geojson',
 // Point to GeoJSON data. This example visualizes all M1.0+ earthquakes
 // from 12/22/15 to 1/21/16 as logged by USGS' Earthquake hazards program.
-data: 'https://docs.mapbox.com/mapbox-gl-js/assets/earthquakes.geojson',
+data: events,
 cluster: true,
 clusterMaxZoom: 14, // Max zoom to cluster points on
 clusterRadius: 50 // Radius of each cluster when clustering points (defaults to 50)
